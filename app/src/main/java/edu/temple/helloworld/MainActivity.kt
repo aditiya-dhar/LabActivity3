25688941383b655f068507cdac1e94d3581f4674
adi <tut25650@temple.edu>
2025-02-05 16:30:27 -0500

    Resolved Issue #2 - feature request

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -20,7 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if (findViewById<EditText>(R.id.nameEditText).text.toString() == null)
+            {
+                findViewById<EditText>(R.id.nameEditText).error = "Please enter a name."
+            }
+            else {
+                displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            }
         }
 
 
